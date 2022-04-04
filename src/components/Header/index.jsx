3cbb4878f8af190d3ca88246e67941690a028418
Fabien Chareun <cacrfabfr@gmail.com>
2022-04-04 14:54:38 +0200

    ex3

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,9 @@
-import { Link } from "react-router-dom"
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
     <nav>
-      <Link to="/">Accueil</Link> |{" "}
-      <Link to="/survey/1">Questionnaire</Link> |{" "}
+      <Link to="/">Accueil</Link> | <Link to="/survey/1">Questionnaire</Link> |{' '}
       <Link to="/freelances">Freelances</Link>
     </nav>
   )
