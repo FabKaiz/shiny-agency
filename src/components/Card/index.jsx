b1729e992c99677edb029a9a2ec2e02d3a592236
Fabien Chareun <cacrfabfr@gmail.com>
2022-04-06 15:15:26 +0200

    css

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -3,29 +3,51 @@ import styled from 'styled-components'
 import colors from '../../utils/style/colors'
 
 const CardLabel = styled.span`
-  color: #5843e4;
+  color: #fff;
   font-size: 22px;
   font-weight: bold;
+  text-align: start;
+  margin-left: 10px;
+  
+`
+
+const CardImageDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: ${colors.backgroundDarkSecondary};
+
 `
 
 const CardImage = styled.img`
-  height: 80px;
-  width: 80px;
+  height: 148px;
+  width: 148px;
   border-radius: 50%;
+`
+
+const CardName = styled.span`
+  text-align: center;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 25px;
+  line-height: 29px;
+  color: #fff;
 `
 
 const CardWrapper = styled.div`
   display: flex;
   flex-direction: column;
   padding: 15px;
-  background-color: ${colors.backgroundLight};
+  background-color: ${colors.backgroundDarkSecondary};
   border-radius: 30px;
-  width: 350px;
+  width: 300px;
+  height: 300px;
   transition: 200ms;
   color: black;
+  justify-content: space-around;
   &:hover {
     cursor: pointer;
-    box-shadow: 2px 2px 10px #e2e3e9;
+    box-shadow: 2px 2px 10px #15141e;
   }
 `
 
@@ -33,8 +55,10 @@ const Card = ({ label, title, picture }) => {
   return (
     <CardWrapper>
       <CardLabel>{label}</CardLabel>
-      <CardImage src={picture} alt="freelance" />
-      <span>{title}</span>
+      <CardImageDiv>
+        <CardImage src={picture} alt="freelance" />
+      </CardImageDiv>
+      <CardName>{title}</CardName>
     </CardWrapper>
   )
 }
--- src/pages/Freelances/index.jsx
+++ src/pages/Freelances/index.jsx
@@ -1,25 +1,8 @@
-import DefaultPicture from '../../assets/profile.png'
 import Card from '../../components/Card'
 import './freelances.css'
 import styled from 'styled-components'
-
-const freelanceProfiles = [
-  {
-    name: 'Jane Doe',
-    jobTitle: 'Devops',
-    picture: DefaultPicture,
-  },
-  {
-    name: 'John Doe',
-    jobTitle: 'Developpeur frontend',
-    picture: DefaultPicture,
-  },
-  {
-    name: 'Jeanne Biche',
-    jobTitle: 'Développeuse Fullstack',
-    picture: DefaultPicture,
-  },
-]
+import { useEffect, useState } from 'react'
+import { Loader } from '../../utils/style/Atoms'
 
 const StyledH4 = styled.h4`
   margin: 0;
@@ -38,11 +21,53 @@ const StyledH5 = styled.h5`
 const CardsContainer = styled.div`
   display: grid;
   gap: 24px;
-  grid-template-rows: 350px 350px;
   grid-template-columns: repeat(2, 1fr);
+  align-items: center;
+  justify-items: center;
+  margin-bottom: 76px;
+
+  
+  @media (max-width: 738px) {
+    grid-template-columns: repeat(1, 1fr);
+  }
+
+  @media (min-width: 1368px) {
+    grid-template-columns: repeat(3, 1fr);
+  }
+
+  
+  @media (min-width: 1568px) {
+    grid-template-columns: repeat(4, 1fr);
+  }
 `
 
 const Freelances = () => {
+  const [isLoading, setIsLoading] = useState(false)
+  const [profilData, setProfilData] = useState([])
+  const [error, setError] = useState(false)
+
+  async function fetchProfilData() {
+    setIsLoading(true)
+    try {
+      const response = await fetch(`http://localhost:8000/freelances`)
+      const { freelancersList } = await response.json()
+      setProfilData(freelancersList)
+    } catch (error) {
+      console.log('===== error =====', error)
+      setError(true)
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    fetchProfilData()
+  }, [])
+
+  if (error) {
+    return <span>Oups il y a eu un problème</span>
+  }
+
   return (
     <div className="Freelances">
       <StyledH5>
@@ -50,14 +75,27 @@ const Freelances = () => {
       </StyledH5>
       <StyledH4>Trouvez votre prestataire</StyledH4>
       <CardsContainer>
-        {freelanceProfiles.map((profile, index) => (
+        {isLoading ? (
+          <Loader />
+        ) : (
+          profilData.map((profile) => (
+            <Card
+              key={`${profile.name}-${profile.id}`}
+              label={profile.job}
+              picture={profile.picture}
+              title={profile.name}
+            />
+          ))
+        )}
+
+        {/* {freelanceProfiles.map((profile, index) => (
           <Card
             key={`${profile.name}-${index}`}
             label={profile.jobTitle}
             picture={profile.picture}
             title={profile.name}
           />
-        ))}
+        ))} */}
       </CardsContainer>
     </div>
   )
