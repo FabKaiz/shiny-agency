a2727db35cba891a6dd1cec29e1dd90542339ecc
Fabien Chareun <cacrfabfr@gmail.com>
2022-04-06 15:26:49 +0200

    css media queries

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -12,18 +12,35 @@ const StyledHome = styledComponents.div`
   justify-content: space-evenly;
   padding: 139px 77px;
   margin: 62px 62px 0 62px;
+
+  @media (max-width: 930px) {
+    flex-direction: column-reverse;
+    justify-content: flex-end;
+  }
 `
 
 const StyledHomeLeft = styledComponents.div`
   background-color: ${colors.backgroundDarkSecondary};
   max-height: 500px;
   max-width: calc(44vw - 139px);
+
+  @media (max-width: 930px) {
+    max-width: calc(100vw - 139px);
+  }
 `
 
 const StyledHomeRight = styledComponents.div`
   background-color: ${colors.backgroundDarkSecondary};
   max-height: 500px;
   max-width: calc(44vw - 139px);
+
+  @media (max-width: 930px) {
+    display: flex;
+    max-width: calc(100vw - 139px);
+    justify-content: center;
+    align-items: center;
+    margin-bottom 1rem
+  }
 `
 
 const StyledLink = styledComponents(Link)`
@@ -35,6 +52,15 @@ const StyledLink = styledComponents(Link)`
     props.$isFullLink &&
     `color: white; border-radius: 10px; background-color: ${colors.primary};`}
  }
+
+ @media (max-width: 930px) {
+  display: flex;
+  max-width: 100%;
+  justify-content: center;
+  align-items: center;
+  padding: 15px 5px;
+
+}
 `
 
 function Home() {
