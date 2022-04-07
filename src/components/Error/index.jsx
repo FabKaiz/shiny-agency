cdf878e6eeede964d064d6779de62c4ef09b68d0
Fabien Chareun <cacrfabfr@gmail.com>
2022-04-07 13:08:17 +0200

    media queries

--- src/components/Error/index.jsx
+++ src/components/Error/index.jsx
@@ -4,13 +4,21 @@ import errorLogo from '../../assets/404.svg'
 
 const ErrorWrapper = styled.div`
   background-color: ${colors.backgroundDarkSecondary};
-  height: calc(100vh - 149px);
+  max-height: calc(100vh - 549px);
   width: calc(100vw - 120px);
   margin: 60px;
   display: flex;
   flex-direction: column;
   align-items: center;
+  justify-content: center;
   color: #fff;
+  padding: 70px 0;
+
+  @media (max-width: 450px) {
+    margin: 0;
+    width: 100vw;
+    max-height: 100vh;
+  }
 `
 
 const ErrorTitle = styled.h1`
@@ -26,7 +34,7 @@ const ErrorSubtitle = styled.h2`
 `
 
 const Illustration = styled.img`
-  max-width: 800px;
+  max-height: 250px;
   width: 80%;
 `
 
